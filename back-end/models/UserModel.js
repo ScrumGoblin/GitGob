6443ac00e5ceaf949e7c042fa4980ebe11f5319a
Nancy Zhang <zhangn356@gmail.com>
2023-01-21 14:37:24 -0500

    Mongoose Model

--- back-end/models/UserModel.js
+++ back-end/models/UserModel.js
@@ -1,19 +1,19 @@
-const { Pool } = require('pg');
+const mongoose = require('mongoose'); 
 
-const connectionString =
-  'postgres://fqslfsxx:0zJzt8o_bIJGff2Rk-wj9ud7syByD1-a@ruby.db.elephantsql.com/fqslfsxx';
-
-// create a new pool here using the connection string above
-const pool = new Pool({
-  connectionString,
-});
-
-// We export an object that contains a property called query,
-// which is a function that returns the invocation of pool.query() after logging the query
-// This will be required in the controllers to be the access point to the database
-module.exports = {
-  query: (text, params, callback) => {
-    console.log('executed query', text);
-    return pool.query(text, params, callback);
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
   },
-};
+  password: {
+    type: String
+  }, 
+  //name of all the repos
+  projects: {
+    type: [String],
+    required: true
+  }
+}); 
+
+const User = mongoose.model('user', userSchema)
+module.exports = User;
